cbb4a5fe505747c2c4f1b3750db96a1e412d8747
Krish Shah <shahkrish2016@gmail.com>
2024-08-26 19:46:24 -0400

    combined individual parsers into `extractData` that accepts text string as a param

--- src/parser.js
+++ src/parser.js
@@ -3,89 +3,92 @@ import * as cheerio from 'cheerio';
 import assert from 'assert';
 
 // use trim
-// cut out the ':' at the end
+// cut out the ':' at the end - instead of slicing off indiscriminately, we should check if the last character is a colon
 const cleanText = (text) => {
-    return text.trim().slice(0, -1);
+    let cleanedText = text.trim();
+
+    if (cleanedText[cleanedText.length - 1] === ':') {
+        cleanedText = cleanedText.slice(0, -1);
+    }
+
+    return cleanedText;
 }
 
 let text = fs.readFileSync('src/sample-posting.html', 'utf8');
 
-const $ = cheerio.load(text);
-
-const postingDiv = $('#postingDiv');
-const panels = postingDiv.find('.panel.panel-default');
-
-assert(panels.length === 3);
-
 // TODO: set up a dataclass (return header)
-// parse first div
+const extractData = (text) => {
+    const $ = cheerio.load(text);
+    const postingDiv = $('#postingDiv');
+    const panels = postingDiv.find('.panel.panel-default');
 
-const extractFirstPanelData = (panels) => {
-    const panel = panels.eq(0);
-    const header = panel.find('.panel-heading').text().trim();
-    const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
+    assert(panels.length === 3);
 
-    assert(rowData.length === 14);
-    const data = {};
+    // parse first panel
+    const extractFirstPanelData = () => {
+        const panel = panels.eq(0);
+        const header = panel.find('.panel-heading').text().trim();
+        const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
 
-    for (let i = 0; i < rowData.length; i++) {
-        const currentRow = rowData.eq(i);
-        const label = $(currentRow).find('td').eq(0).text().trim(); 
-        const value = $(currentRow).find('td').eq(1).text().trim(); 
-        data[cleanText(label)] = cleanText(value);
+        assert(rowData.length === 14);
+        const data = {};
+
+        for (let i = 0; i < rowData.length; i++) {
+            const currentRow = rowData.eq(i);
+            const label = $(currentRow).find('td').eq(0).text().trim(); 
+            const value = $(currentRow).find('td').eq(1).text().trim(); 
+            data[cleanText(label)] = cleanText(value);
+        }
+
+        return data;
     }
+
+
+    // parse second panel
+    const extractSecondPanelData = () => {
+        const panel = panels.eq(1);
+        const header = panel.find('.panel-heading').text().trim();
+        const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
+
+        assert(rowData.length === 4);
+        const data = {};
+
+        for (let i = 0; i < rowData.length; i++) {
+            const currentRow = rowData.eq(i);
+            const label = $(currentRow).find('td').eq(0).text().trim(); 
+            const value = $(currentRow).find('td').eq(1).text().trim(); 
+            data[cleanText(label)] = cleanText(value);
+        }
+
+        return data;
+    }
+
+
+    // parse third panel
+    const extractThirdPanelData = () => {
+        const panel = panels.eq(2);
+        const header = panel.find('.panel-heading').text().trim();
+        const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
+
+        assert(rowData.length === 2);
+        const data = {};
+
+        for (let i = 0; i < rowData.length; i++) {
+            const currentRow = rowData.eq(i);
+            const label = $(currentRow).find('td').eq(0).text().trim(); 
+            const value = $(currentRow).find('td').eq(1).text().trim(); 
+            data[cleanText(label)] = cleanText(value);
+        }
+
+        return data;
+    }
+
+    const data = Object.assign({}, extractFirstPanelData(panels), extractSecondPanelData(panels), extractThirdPanelData(panels));
 
     return data;
 }
 
-const firstDivData = extractFirstPanelData(panels);
-console.log(Object.keys(firstDivData));
-
-
-// parse second div
-
-const extractSecondPanelData = (panels) => {
-    const panel = panels.eq(1);
-    const header = panel.find('.panel-heading').text().trim();
-    const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
-
-    assert(rowData.length === 4);
-    const data = {};
-
-    for (let i = 0; i < rowData.length; i++) {
-        const currentRow = rowData.eq(i);
-        const label = $(currentRow).find('td').eq(0).text().trim(); 
-        const value = $(currentRow).find('td').eq(1).text().trim(); 
-        data[cleanText(label)] = cleanText(value);
-    }
-
-    return data;
-}
-
-const secondDivData = extractSecondPanelData(panels);
-console.log(Object.keys(secondDivData));
-
-
-// parse third div
-
-const extractThirdPanelData = (panels) => {
-    const panel = panels.eq(2);
-    const header = panel.find('.panel-heading').text().trim();
-    const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
-
-    assert(rowData.length === 2);
-    const data = {};
-
-    for (let i = 0; i < rowData.length; i++) {
-        const currentRow = rowData.eq(i);
-        const label = $(currentRow).find('td').eq(0).text().trim(); 
-        const value = $(currentRow).find('td').eq(1).text().trim(); 
-        data[cleanText(label)] = cleanText(value);
-    }
-
-    return data;
-}
-
-const thirdDivData = extractThirdPanelData(panels);
-console.log(Object.keys(thirdDivData));
+const data = extractData(text);
+console.log(Object.keys(data));
+console.log(Object.keys(data).length);
 
