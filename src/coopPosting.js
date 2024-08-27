ecd0d2648e97acba2b933916aff44e3d89505890
Krish Shah <shahkrish2016@gmail.com>
2024-08-26 20:22:15 -0400

    created `CoopPosting` class as a dataclass

---
+++ src/coopPosting.js
@@ -0,0 +1,63 @@
+
+// use trim
+// cut out the ':' at the end - instead of slicing off indiscriminately, we should check if the last character is a colon
+const cleanText = (text) => {
+    let cleanedText = text.trim();
+    if (cleanedText[cleanedText.length - 1] === ':') {
+        cleanedText = cleanedText.slice(0, -1);
+    }
+
+    return cleanedText;
+}
+
+
+
+// dataclass for a coop posting
+class CoopPosting {
+    
+    // process each individual field 
+    constructor(termPosted,
+                positionType,
+                level,
+                jobTitle,
+                jobOpenings,
+                jobCategory,
+                region,
+                startDate,
+                jobSummary,
+                jobResponsibilities,
+                requiredSkills,
+                compensationAndBenefits,
+                otherJobDetails,
+                targetedDegreesAndDisciplines,
+                applicationDeadline,
+                applicatonDelivery,
+                applicationDocumentsRequired,
+                additionalApplicationInfo,
+                organization,
+                division) {
+        this.termPosted = termPosted;
+        this.positionType = positionType;
+        this.level = level;
+        this.jobTitle = jobTitle;
+        this.jobOpenings = jobOpenings;
+        this.jobCategory = jobCategory;
+        this.region = region;
+        this.startDate = startDate;
+        this.jobSummary = jobSummary;
+        this.jobResponsibilities = jobResponsibilities;
+        this.requiredSkills = requiredSkills;
+        this.compensationAndBenefits = compensationAndBenefits;
+        this.otherJobDetails = otherJobDetails;
+        this.targetedDegreesAndDisciplines = targetedDegreesAndDisciplines;
+        this.applicationDeadline = applicationDeadline;
+        this.applicatonDelivery = applicatonDelivery;
+        this.applicationDocumentsRequired = applicationDocumentsRequired;
+        this.additionalApplicationInfo = additionalApplicationInfo;
+        this.organization = organization;
+        this.division = division;
+    }
+
+}
+
+export default CoopPosting;
--- src/parser.js
+++ src/parser.js
@@ -1,22 +1,11 @@
+// set up TS for type signatures on functions
 import fs from 'fs';
 import * as cheerio from 'cheerio';
 import assert from 'assert';
-
-// use trim
-// cut out the ':' at the end - instead of slicing off indiscriminately, we should check if the last character is a colon
-const cleanText = (text) => {
-    let cleanedText = text.trim();
-
-    if (cleanedText[cleanedText.length - 1] === ':') {
-        cleanedText = cleanedText.slice(0, -1);
-    }
-
-    return cleanedText;
-}
+import CoopPosting from './coopPosting.js';
 
 let text = fs.readFileSync('src/sample-posting.html', 'utf8');
 
-// TODO: set up a dataclass (return header)
 const extractData = (text) => {
     const $ = cheerio.load(text);
     const postingDiv = $('#postingDiv');
@@ -27,7 +16,7 @@ const extractData = (text) => {
     // parse first panel
     const extractFirstPanelData = () => {
         const panel = panels.eq(0);
-        const header = panel.find('.panel-heading').text().trim();
+        //const header = panel.find('.panel-heading').text().trim();
         const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
 
         assert(rowData.length === 14);
@@ -37,7 +26,7 @@ const extractData = (text) => {
             const currentRow = rowData.eq(i);
             const label = $(currentRow).find('td').eq(0).text().trim(); 
             const value = $(currentRow).find('td').eq(1).text().trim(); 
-            data[cleanText(label)] = cleanText(value);
+            data[label] = value;
         }
 
         return data;
@@ -47,7 +36,7 @@ const extractData = (text) => {
     // parse second panel
     const extractSecondPanelData = () => {
         const panel = panels.eq(1);
-        const header = panel.find('.panel-heading').text().trim();
+        //const header = panel.find('.panel-heading').text().trim();
         const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
 
         assert(rowData.length === 4);
@@ -57,7 +46,7 @@ const extractData = (text) => {
             const currentRow = rowData.eq(i);
             const label = $(currentRow).find('td').eq(0).text().trim(); 
             const value = $(currentRow).find('td').eq(1).text().trim(); 
-            data[cleanText(label)] = cleanText(value);
+            data[label] = value;
         }
 
         return data;
@@ -67,7 +56,7 @@ const extractData = (text) => {
     // parse third panel
     const extractThirdPanelData = () => {
         const panel = panels.eq(2);
-        const header = panel.find('.panel-heading').text().trim();
+        //const header = panel.find('.panel-heading').text().trim();
         const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
 
         assert(rowData.length === 2);
@@ -77,18 +66,25 @@ const extractData = (text) => {
             const currentRow = rowData.eq(i);
             const label = $(currentRow).find('td').eq(0).text().trim(); 
             const value = $(currentRow).find('td').eq(1).text().trim(); 
-            data[cleanText(label)] = cleanText(value);
+            data[label] = value;
         }
 
         return data;
     }
 
-    const data = Object.assign({}, extractFirstPanelData(panels), extractSecondPanelData(panels), extractThirdPanelData(panels));
+    const data = Object.assign(
+        {}, 
+        extractFirstPanelData(panels), 
+        extractSecondPanelData(panels), 
+        extractThirdPanelData(panels)
+    );
 
     return data;
 }
 
 const data = extractData(text);
-console.log(Object.keys(data));
-console.log(Object.keys(data).length);
+
+const values = Object.values(data);
+const posting = new CoopPosting(...values);
+console.log(posting);
 
