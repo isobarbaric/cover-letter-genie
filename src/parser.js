ddc9af5ef37d4ddd33496f10791d79df602dfd21
Krish Shah <shahkrish2016@gmail.com>
2024-08-26 20:42:46 -0400

    combined three redundant functions into a nested for loop

--- src/parser.js
+++ src/parser.js
@@ -12,79 +12,29 @@ const extractData = (text) => {
     const panels = postingDiv.find('.panel.panel-default');
 
     assert(panels.length === 3);
+    const data = {};
 
-    // parse first panel
-    const extractFirstPanelData = () => {
-        const panel = panels.eq(0);
-        //const header = panel.find('.panel-heading').text().trim();
+    // iterate through each panel
+    for (let i = 0; i < panels.length; i++) {
+        const panel = panels.eq(i);
         const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
 
-        assert(rowData.length === 14);
-        const data = {};
-
+        // in each panel, iterate through each row
         for (let i = 0; i < rowData.length; i++) {
             const currentRow = rowData.eq(i);
             const label = $(currentRow).find('td').eq(0).text().trim(); 
             const value = $(currentRow).find('td').eq(1).text().trim(); 
             data[label] = value;
         }
-
-        return data;
     }
 
-
-    // parse second panel
-    const extractSecondPanelData = () => {
-        const panel = panels.eq(1);
-        //const header = panel.find('.panel-heading').text().trim();
-        const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
-
-        assert(rowData.length === 4);
-        const data = {};
-
-        for (let i = 0; i < rowData.length; i++) {
-            const currentRow = rowData.eq(i);
-            const label = $(currentRow).find('td').eq(0).text().trim(); 
-            const value = $(currentRow).find('td').eq(1).text().trim(); 
-            data[label] = value;
-        }
-
-        return data;
-    }
-
-
-    // parse third panel
-    const extractThirdPanelData = () => {
-        const panel = panels.eq(2);
-        //const header = panel.find('.panel-heading').text().trim();
-        const rowData = panel.find('.panel-body > .table.table-bordered > tbody > tr');
-
-        assert(rowData.length === 2);
-        const data = {};
-
-        for (let i = 0; i < rowData.length; i++) {
-            const currentRow = rowData.eq(i);
-            const label = $(currentRow).find('td').eq(0).text().trim(); 
-            const value = $(currentRow).find('td').eq(1).text().trim(); 
-            data[label] = value;
-        }
-
-        return data;
-    }
-
-    const data = Object.assign(
-        {}, 
-        extractFirstPanelData(panels), 
-        extractSecondPanelData(panels), 
-        extractThirdPanelData(panels)
-    );
-
+    assert(Object.keys(data).length === 20);
     return data;
 }
 
 const data = extractData(text);
+console.log(Object.keys(data));
 
 const values = Object.values(data);
 const posting = new CoopPosting(...values);
-console.log(posting);
-
+console.log(posting.toString());
