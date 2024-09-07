73afd557559797d6ebc39199815e608a7115144c
Krish Shah <shahkrish2016@gmail.com>
2024-09-07 15:37:57 -0400

    simplified code to only require HTML page for validation and not have to use `onActivated` & `onUpdated`

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,27 @@
 import extractData from './parser.js';
 
-const WATERLOO_WORKS_URL = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/full/jobs.htm";
+// check if all values in the object are undefined
+const isPosting = (obj) => {
+  for (const key in obj) {
+    if (obj[key] !== undefined) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let iconPath = 'black-icon16.png';
+
+const toggleIcon = (postingOpen) => {
+  if (postingOpen) {
+    iconPath = 'blue-icon16.png';
+  } else {
+    iconPath = 'black-icon16.png';
+  }
+
+  chrome.action.setIcon({ path: iconPath });
+}
 
 //chrome.runtime.onInstalled.addListener(({reason}) => {
 //  if (reason === 'install') {
@@ -10,77 +31,21 @@ const WATERLOO_WORKS_URL = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/f
 //  }
 //});
 
-// TODO: save global variable for the current tab URL for extra validation in the onMessage listener
 
-// fires when the active tab changes
-// can't use activeInfo directly since it doesn't contain url info
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  // when the URL changes (new page opened)
-  if (changeInfo.url) {
-    console.log('Tab updated with new URL:', changeInfo.url);
-
-    let currentURL = changeInfo.url;
-
-    console.log('Updating icon based on URL:', currentURL);
-    try {
-      updateIconBasedOnUrl(currentURL);
-      console.log('Icon updated based on new URL');
-    } catch (error) {
-      console.error('Failed to update icon:', error);
-    }
-  }
-});
-
-//chrome.tabs.onActivated.addListener(async (activeInfo) => {
-//  const tabId = activeInfo.tabId;
-//  //console.log('Tab activated:', tabId);
-//
-//  let currentURL = null;
-//  try {
-//    const tab = await chrome.tabs.get(tabId);
-//    currentURL = tab.url;
-//    console.log('Current URL:', currentURL);
-//  } catch (error) {
-//    console.error('Failed to get tab information:', error);
-//  }
-//
-//  //console.log("Updating icon based on URL:", currentURL);
-//  try {
-//    updateIconBasedOnUrl(currentURL);
-//    console.log("Icon updated based on URL");
-//  } catch (error) {
-//    console.error('Failed to update icon:', error);
-//  }
-//});
-//
-
-let iter = 1;
-
+// TODO: have content script send html without having to reload the page 
 // listen for messages from the content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("iter number:", iter);
-  iter += 1;
   if (message.type === 'page_html') {
-    console.log("Received message from content script:", message);
     const parsedData = extractData(message.html);
-    console.log("Parsed data:", parsedData);
+    console.log(parsedData);
+
+    if (isPosting(parsedData)) {
+      console.log("No data found in the page");
+      toggleIcon(false);
+      return;
+    }
+
+    console.log("posting happened");
+    toggleIcon(true);
   }
 });
-
-
-//chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-//  if (changeInfo.status === 'complete') {
-//    currentTabUrl = tab.url;
-//    chrome.runtime.sendMessage({ type: 'TAB_UPDATED', url: currentTabUrl });
-//  }
-//});
-
-
-function updateIconBasedOnUrl(url) {
-  let iconPath = 'black-icon16.png';
-  if (url.includes(WATERLOO_WORKS_URL)) {
-    iconPath = 'blue-icon16.png';
-  } 
-  chrome.action.setIcon({ path: iconPath });
-}
-
--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,4 @@
-// TODO: get this to only fire when the user is on the WaterlooWorks page
-
 const pageHtml = document.documentElement.outerHTML;
-console.log(pageHtml);
 
 // send the html to the background script
 chrome.runtime.sendMessage({ type: 'page_html', html: pageHtml });
