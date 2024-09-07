74d0300f0a21dda718f753b48573e38230fa2c2f
Krish Shah <shahkrish2016@gmail.com>
2024-09-07 15:46:37 -0400

    minor cleanup

--- extension/background.js
+++ extension/background.js
@@ -4,11 +4,11 @@ import extractData from './parser.js';
 const isPosting = (obj) => {
   for (const key in obj) {
     if (obj[key] !== undefined) {
-      return false;
+      return true;
     }
   }
 
-  return true;
+  return false;
 }
 
 let iconPath = 'black-icon16.png';
@@ -23,29 +23,27 @@ const toggleIcon = (postingOpen) => {
   chrome.action.setIcon({ path: iconPath });
 }
 
-//chrome.runtime.onInstalled.addListener(({reason}) => {
-//  if (reason === 'install') {
-//    chrome.tabs.create({
-//      url: "onboarding.html"
-//    });
-//  }
-//});
-
+chrome.runtime.onInstalled.addListener(({reason}) => {
+  if (reason === 'install') {
+    console.log("WaterlooWorks Cover Letter Genie (V1.0)");
+    //chrome.tabs.create({
+    //  url: "onboarding.html"
+    //});
+  }
+});
 
 // TODO: have content script send html without having to reload the page 
 // listen for messages from the content script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'page_html') {
     const parsedData = extractData(message.html);
-    console.log(parsedData);
 
     if (isPosting(parsedData)) {
-      console.log("No data found in the page");
+      toggleIcon(true);
+      console.log(parsedData);
+    } else {
       toggleIcon(false);
-      return;
+      console.log('No posting detected');
     }
-
-    console.log("posting happened");
-    toggleIcon(true);
   }
 });
